8339e0cd65c76f58d278aad125f0905e7c20061e
Nathaniel R. Lewis <nlewis@gaikai.com>
2015-07-02 14:00:15 -0700

    Working on methods (using new any type)

--- test.cpp
+++ test.cpp
@@ -1,177 +1,160 @@
 #include <iostream>
+#include <vector>
+#include <exception>
+#include <stdexcept>
+#include <tuple>
+#include <string>
 #include <functional>
 
-//#include <string>
-//#include <map>
-
-//#include <boost/thread/shared_mutex.hpp>
-
-//#include "common.hpp"
 #include "any.hpp"
 #include "type.hpp"
-//#include "variant.hpp"
 
 using namespace reflectioncpp;
 using namespace std;
 
-/*class MetaType
+// Method invoke base class
+/*class Method
 {
 public:
-	virtual std::string Name() const = 0;
-	virtual size_t Size() const = 0;
-	virtual TypeCode Identifier() const = 0;
-};
-
-template <typename T>
-class MetaTypeImpl : public MetaType
-{
-public:
-	size_t Size() const override
-	{
-		return sizeof(T);
-	}
-
-	std::string Name() const override
-	{
-		return Type<T>::Name();
-	}
-
-	TypeCode Identifier() const override
-	{
-		return Type<T>::Code();
-	}
-};
-
-class MetaTypeDatabase
-{
-	std::map<TypeCode, MetaType *> database;
-	boost::shared_mutex databaseAccess;
-
-	static MetaTypeDatabase& Instance()
-	{
-		static MetaTypeDatabase instance;
-		return instance;
-	}
-
-public:
-	static void RegisterType(MetaType *type)
-	{
-		boost::unique_lock<boost::shared_mutex> lock(Instance().databaseAccess);
-		Instance().database[type->Identifier()] = type;
-	}
-
-	static MetaType* GetType(TypeCode typeId)
-	{
-		boost::shared_lock<boost::shared_mutex> lock(Instance().databaseAccess);
-		return Instance().database[typeId];
-	}
-
-	template <class T>
-	static MetaType* Get()
-	{
-		using TypeInfo = typename reflectioncpp::Type<T>;
-
-		boost::shared_lock<boost::shared_mutex> lock(Instance().databaseAccess);
-		return Instance().database[TypeInfo::Code()];
-	}
+    // Can we use templates to handle the void return case
+    virtual Variant Invoke(void *instance, vector<Variant> params) const = 0;
+    virtual Variant Invoke(void *instance) const = 0;
 };*/
 
-struct Herp
-{
-	int a;
-	char b;
-	float c;
+// Method invoke implementation
+template<class,class>
+class MethodImpl;
 
-	virtual void doSomething()
-	{
-		cout << a << " " << b << " " << c << endl;
-	}
+template<class T, class R, class... Args>
+class MethodImpl<T, R(Args...)>
+{
+public:
+    typedef R (T::*PointerType)(Args...);
+    PointerType methodPointer;
+public:
+
+    // Internal invoker methods
+    R Invoke(void *instance)
+    {
+        // Invoke the method
+        return (((T*) instance)->*methodPointer)();
+    }
+
+    // Completed parameter pack, executes method
+    template <typename... Ts>
+    typename enable_if<sizeof...(Args) == sizeof...(Ts), R>::type
+    Invoke(void *instance, vector<any>& v, Ts&&... ts)
+    {
+        if(sizeof...(Ts) > v.size())
+            throw out_of_range("vector too small for function");
+
+        // Invoke the method
+        return (((T*) instance)->*methodPointer)(std::forward<Ts>(ts)...);
+    }
+
+    // Builds the parameter pack
+    template <typename... Ts>
+    typename enable_if<sizeof...(Args) != sizeof...(Ts), R>::type
+    Invoke(void *instance, vector<any>& v, Ts&&... ts)
+    {
+        const int index = sizeof...(Args) - sizeof...(Ts) - 1;
+        static_assert(index >= 0, "incompatible function parameters");
+
+        using type = typename tuple_element<index, tuple<Args...>>::type;
+
+        cout << "Unpacking: " << Type<type>::Info().name << " @" << index << endl;
+
+        return Invoke(instance, 
+        			  v, 
+        			  any_cast<type>(*(std::begin(v) + index)), 
+        			  std::forward<Ts>(ts)... 
+        			 );
+    }
+
+    // Add code (using enable_if<is_void stuff) to marshall void calls to variant returns
+    /*template <typename U = R>
+    typename enable_if<!is_void<U>::value, Variant>::type
+    InvokeInternal(void *instance, const vector<Variant>& params)
+    {
+
+    }*/
+
+public:
+    MethodImpl(PointerType methodPointer)
+        : methodPointer(methodPointer)
+    {
+
+    }
+
+    /// DO NOT USE YET (VERY BAD POINTER TO LOCAL THING) JUST HERE SO SHIT COMPILES
+    /*Variant Invoke(void *instance, vector<Variant> params) const
+    {
+        // Invoke the method
+        R result = Invoke(instance, params);
+        return Variant(&result);
+    }
+
+    /// DO NOT USE YET (VERY BAD POINTER TO LOCAL THING) JUST HERE SO SHIT COMPILES
+    Variant Invoke(void *instance) const
+    {
+        // Invoke the method
+        R result = Invoke(instance);
+        return Variant(&result);
+    }*/
 };
 
-DEFINE_TYPE(Herp, Herp);
+class A
+{
+public:
+    int value;
 
-DEFINE_TYPE(std::reference_wrapper<Herp>, Herp_ref);
+public:
+    A() : value(0) {}
+
+    void Set(int a) {value = a;}
+    int Get() {return value;}
+    void Increment() {value++;};
+
+    static void Herp()
+    {
+        std::cout << "Herp Derp" << std::endl;
+    }
+    void Derp(int a, float b)
+    {
+    	cout << a << " " << b << endl;
+    }
+
+    int operator()()
+    {
+        return value;
+    }
+};
+
+DEFINE_TYPE(A, A);
+DEFINE_TYPE(string, string);
 
 int main (int argc, char** argv)
 {
-	/*reflectioncpp::Variant a;
-	
-	int aNumber = 4;
-	a.Set(aNumber);
-	a = 4;
+    MethodImpl<A, void(int)>  _set(&A::Set);
+    MethodImpl<A, void(int,float)> _derp(&A::Derp);
+    MethodImpl<A, int()>  _get(&A::Get);
+    MethodImpl<A, void()> _increment(&A::Increment);
+    MethodImpl<A, int()> _function(&A::operator());
 
-	std::cout << "addr in parent: " << (size_t) &aNumber << std::endl;
-	a.SetReference(aNumber);
+    A test;
+    any v = 5;
+    any q = 5.6f;
 
-	//a.Set(5);
-	//int f = 3;
-	//int* g = &f;
-	//a = g;
+    cout << "value: " << _get.Invoke((void *) &test) << endl;
 
-	//reflectioncpp::Variant b;
-	//b.Set(std::string("hello world"));*/
+    vector<any> args = {v, q};
+    _set.Invoke((void *) &test, args);
 
-	//MetaTypeDatabase::RegisterType(new MetaTypeImpl<Herp>);
-	//MetaTypeDatabase::RegisterType(new MetaTypeImpl<int>);
+    _derp.Invoke((void *) &test, args);
 
-	/*Herp d;
-	d.a = 1;
-	d.b = 'c';
-	d.c = 3.14159f;
-
-	any herpV(d);
-
-	any test;
-	test = std::ref(d);
-
-
-	Herp& refH = any_cast<Herp&>(herpV);
-	refH.c = 6.242f;
-
-	cout << d.c << " " << refH.c << endl;
-
-	Herp& refH2 = any_cast<Herp&>(herpV);
-	refH.c = 9.242f;
-
-	cout << refH.c << " " << refH2.c << endl << endl;
-
-	Herp& refH3 = any_cast< decltype((std::ref(d))) >(test).get();
-
-	cout << d.c << " " << refH3.c << endl;
-	refH3.c = 42.1f;
-	cout << d.c << " " << refH3.c << endl;
-
-
-	cout << "type: " << herpV.type().name << endl;
-	cout << "type: " << test.type().name << endl;
-
-	const volatile int* der_rel;
-
-	const volatile int*& der = der_rel;
-
-	cout << "derp: " << std::is_same<decltype(der_rel), decltype(der)>::value << endl;
-	cout << "derp: " << std::is_reference<decltype(der)>::value << endl;
-	cout << "derp: " << std::is_pointer<typename std::remove_reference<decltype(der)>::type>::value << endl;
-	cout << "derp: " << std::is_const<typename std::remove_pointer<decltype(der)>::type>::value << endl;
-	cout << "derp: " << std::is_volatile<typename std::remove_pointer<decltype(der)>::type>::value << endl;*/
-
-	any aVariant;
-	aVariant = 5;
-
-	int number = any_cast<int>(aVariant);
-
-	cout << "the number = " << number << endl;
-
-	//MetaType *metadata = MetaTypeDatabase::Get<Herp>();
-	//MetaType *integer = MetaTypeDatabase::Get<int>();
-	//TypeInfo info = Type<const volatile int ****>::Info();
-	//TypeInfo info2 = Type<const volatile int ****>::Info();
-
-	//cout << info.name << ": " << info.code << " " << info.isReference << " " << info.indirection << " " << info.isConst << " " << info.isVolatile << " " << (info == info2) << endl;
-
-	//std::cout << integer->Name() << ": " << integer->Identifier() << std::endl;
-
-	//std::cout << metadata->Name() << ": " << metadata->Identifier() << std::endl;
+    cout << "value: " << _get.Invoke((void *) &test) << endl;
+    _increment.Invoke((void *) &test);
+    cout << "value: " << _function.Invoke((void *) &test) << endl;
 
 	return 0;
 }
