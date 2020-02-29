bd31e48f08cd8f88ac2fef93b8776d587a5702c2
Nathaniel R. Lewis <linux.robotdude@gmail.com>
2020-02-29 13:50:23 -0800

    some state i found on my computer years later.

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -56,11 +56,22 @@ public:
     }
 };
 
+class B
+{
+    size_t i = 0;
+public:
+    void DoShit()
+    {
+        i++;
+        //std::cout << "ayy lmao" << std::endl;
+    }
+};
+
 class Accessible
 {
 public:
-    virtual boost::any Get(boost::any instance) = 0;
-    virtual void Set(boost::any instance, boost::any param) = 0;
+    virtual boost::any Get(boost::any instance) const = 0;
+    virtual void Set(boost::any instance, boost::any param) const = 0;
 };
 
 template <class T, typename M, typename Enabled = void>
@@ -70,30 +81,37 @@ template <class T, typename M>
 class Member<T, M, void> : public Accessible
 {
     typedef M T::*PointerType;
-    PointerType memberPointer;
+    const PointerType memberPointer;
 
 public:
-    Member(PointerType memberPointer)
+    constexpr explicit Member(PointerType memberPointer)
         : memberPointer(memberPointer)
     {
     }
 
-    void Set(boost::any instance, boost::any param)
+    void Set(boost::any instance, boost::any param) const
     {
         T* instancePtr = boost::any_cast<T *>(instance);
         instancePtr->*memberPointer = boost::any_cast<M>(param);
     }
 
-    boost::any Get(boost::any instance)
+    boost::any Get(boost::any instance) const
     {
         T* instancePtr = boost::any_cast<T *>(instance);
         return any(instancePtr->*memberPointer);
     }
 };
 
+constexpr static Member<A, int> cAmValue(&A::value);
+
+constexpr static const Accessible *metaMembers[] =
+{
+    &cAmValue,
+};
+
 int main (int argc, char** argv)
 {
-    Invokable *derp      = new MethodImpl<A, int (int,float,string)>(&A::Derp);
+    /*Invokable *derp      = new MethodImpl<A, int (int,float,string)>(&A::Derp);
     Invokable *getValue  = new MethodImpl<A, int& ()>(&A::GetValue);
     Invokable *increment = new MethodImpl<A, void ()>(&A::Increment);
 
@@ -101,9 +119,26 @@ int main (int argc, char** argv)
     Invokable *factoryInitialize = new ConstructorImpl<A* (int)>();
 
     Invokable *staticfunction = new MethodImpl<void, void ()>(&A::Herp);
-    Invokable *static2 = new MethodImpl<void, int& ()>(&A::meaningoflife);
+    Invokable *static2 = new MethodImpl<void, int& ()>(&A::meaningoflife);*/
 
-    Accessible *member = new Member<A, int>(&A::value);
+    Invokable *thingy = new MethodImpl<B, void(void)>(&B::DoShit);
+
+    B instanceOfB;
+    vector<any> thingyArgs = 
+    {
+        &instanceOfB,
+    };
+
+
+    for(size_t i = 0; i < 1000000; i++)
+        thingy->Invoke(thingyArgs);
+
+
+
+
+
+    //Accessible *member = new Member<A, int>(&A::value);
+    /*const Accessible *member = metaMembers[0];
 
     // Test the default constructor
     any aInstance = factoryDefault->Invoke();
@@ -143,7 +178,7 @@ int main (int argc, char** argv)
     any mol = static2->Invoke();
     cout << "type = " << mol.type().name() << endl;
 
-    delete test;
+    delete test;*/
 
 	return 0;
 }
